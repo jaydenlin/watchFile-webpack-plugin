87ddc50e5323cb9bb56247ec805e98d5ebdae8cb
weiyu.lin <weiyu.in@rakuten.com>
2016-01-06 01:47:16 +0800

    format the file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var watch = require('watch');
-var path  = require('path');
-var fs    = require('fs');
-
+var path = require('path');
+var fs = require('fs');
 
 function watchFilePlugin(options) {
   this.options = options;
@@ -11,11 +10,11 @@ watchFilePlugin.prototype.apply = function(compiler) {
   var that = this;
   compiler.plugin("emit", function(compilation, callback) {
     var watchFolder = that.options.watchFolder;
-    watch.createMonitor(watchFolder, function(monitor){
-      monitor.files[path.join(watchFolder, "/*."+that.options.watchExtension)]
-      monitor.on("changed", function (f, curr, prev) {
+    watch.createMonitor(watchFolder, function(monitor) {
+      monitor.files[path.join(watchFolder, "/*." + that.options.watchExtension)]
+      monitor.on("changed", function(f, curr, prev) {
         compiler.run(function(err) {
-          if(err) throw err;
+          if (err) throw err;
           monitor.stop();
         });
       });
